313ab2e9cf86839294f46308bc49b71f8e504859
Monika Rafalska <82111615+MonikaRafalska@users.noreply.github.com>
2022-07-17 21:26:18 +0200

    done

---
+++ js/partners_atuo_hide.js
@@ -0,0 +1,23 @@
+$(function () {
+  var tickerLength = $(".partners ul li").length;
+  var tickerHeight = $(".partners ul li").outerHeight();
+  $(".partners ul li:last-child").prependTo(".partners ul");
+  $(".partners ul").css("marginLeft", -tickerLength);
+  function moveLeft() {
+    if ($(window).width() < 900 || ($(window).width() >= 1200 && $(window).width() <= 1279)) {
+      $(".partners ul").animate(
+        {
+          left: -tickerLength,
+        },
+        600,
+        function () {
+          $(".partners ul li:first-child").appendTo(".partners ul");
+          $(".partners ul").css("left", "");
+        }
+      );
+    }
+  }
+  setInterval(function () {
+    moveLeft();
+  }, 2500);
+});
